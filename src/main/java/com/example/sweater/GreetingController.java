644cc0b5237965842b6526f97da206ab983c698b
KotcherP <chernobai88@gmail.com>
2019-08-22 00:33:54 +0300

    https://www.youtube.com/watch?v=nyFLX3q3poY&list=PLU2ftbIeotGpAYRP9Iv2KLIwK36-o_qYk&index=4
    7:50

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,5 +1,8 @@
 package com.example.sweater;
 
+import com.example.sweater.domain.Message;
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,17 +11,22 @@ import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="Petushok") String name,
                            Map<String,Object> model) {
         model.put("name", name);
         return "greeting";
+
     }
 
     @GetMapping
     public String main(Map<String,Object> model){
-        model.put("some","Ёбана рот");
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages",messages);
         return "main";
     }
 
---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,6 @@
+package com.example.sweater.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepo extends CrudRepository {
+}
